929769a3124a41e5da0939b6b742d6d536496abf
Melissa Filgueira de Souza <programadorimd@hotmail.com>
2025-01-31 16:31:09 -0300

    invertendo string

--- inverter_string/inverter_string.js
+++ inverter_string/inverter_string.js
@@ -1,6 +1,9 @@
 function inverterString() {
+
     const input = document.getElementById('inputString').value;
-    console.log('Entrada', input);
+    const stringInvertida = input.split('').reverse().join('');
+    console.log(stringInvertida)
 
-
+    const resultadoDiv = document.getElementById('resultado');
+    resultadoDiv.innerText = `String invertida: ${stringInvertida}`;
 }
