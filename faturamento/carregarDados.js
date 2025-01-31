1765bd00d588eda71f844599e6b7276517a965d4
Melissa Filgueira de Souza <programadorimd@hotmail.com>
2025-01-31 15:56:32 -0300

    calculando maior faturamento

--- faturamento/carregarDados.js
+++ faturamento/carregarDados.js
@@ -5,8 +5,10 @@ function carregarArquivo() {
             const faturamentoValido = data.filter(dia => dia.valor > 0);
 
             const menorFaturamento = Math.min(...faturamentoValido.map(dia => dia.valor));
+            const maiorFaturamento = Math.max(...faturamentoValido.map(dia => dia.valor));
 
             document.getElementById('menorFaturamento').innerText = `Menor faturamento: ${menorFaturamento}`;
+            document.getElementById('maiorFaturamento').innerText = `Menor faturamento: ${maiorFaturamento}`;
         })
         .catch(error => alert('Erro ao carregar o arquivo JSON: ' + error));
 }
