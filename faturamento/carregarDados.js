cccb0a80bdced022c32601e63c799ffd4d5e970f
Melissa Filgueira de Souza <programadorimd@hotmail.com>
2025-01-31 16:05:41 -0300

    calculando dias acima da média

--- faturamento/carregarDados.js
+++ faturamento/carregarDados.js
@@ -6,9 +6,14 @@ function carregarArquivo() {
 
             const menorFaturamento = Math.min(...faturamentoValido.map(dia => dia.valor));
             const maiorFaturamento = Math.max(...faturamentoValido.map(dia => dia.valor));
+            const totalFaturamento = faturamentoValido.reduce((total, dia) => total + dia.valor, 0);
+            const mediaMensal = totalFaturamento / faturamentoValido.length;
+            const diasAcimaDaMedia = faturamentoValido.filter(dia => dia.valor > mediaMensal).length;
+
 
             document.getElementById('menorFaturamento').innerText = `Menor faturamento: ${menorFaturamento}`;
-            document.getElementById('maiorFaturamento').innerText = `Menor faturamento: ${maiorFaturamento}`;
+            document.getElementById('maiorFaturamento').innerText = `Maior faturamento: ${maiorFaturamento}`;
+            document.getElementById('diasAcimaDaMedia').innerText = `Dias acima da média: ${diasAcimaDaMedia}`;
         })
         .catch(error => alert('Erro ao carregar o arquivo JSON: ' + error));
 }
